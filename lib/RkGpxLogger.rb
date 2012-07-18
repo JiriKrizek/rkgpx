5e5e2d014a666957d88add85cda4141eeb6ffdbf
Jiri Krizek <jkrizek@gmail.com>
2012-07-18 06:48:39 +0200

    Konstruktor Loggeru - pridani levelu logovani

--- lib/RkGpxLogger.rb
+++ lib/RkGpxLogger.rb
@@ -3,12 +3,11 @@ require 'logger'
 class RkGpxLogger < Logger
   FORMAT="%H:%M:%S"
 
-  def initialize(output)
-    super(output)
-    this.level = Logger::DEBUG
-    this.datetime_format = FORMAT
-    this.debug("Starting #{$PROGRAM_NAME}")
+  def initialize(output, lev=Logger::DEBUG)
+  	super(output, 'daily')
+    self.level = lev
+    self.datetime_format = FORMAT
+    self.debug("Starting #{$PROGRAM_NAME}")
   end
-  
   
 end
