4ddfc486daa33bef62a3b382a8996557863970fc
Jiri Krizek <jkrizek@gmail.com>
2012-07-19 19:21:44 +0200

    Main.rb - kod pro ucely testovani

---
+++ lib/main.rb
@@ -0,0 +1,14 @@
+require_relative "RkGpxLogger"
+require_relative "commandline"
+require_relative "gpx"
+require_relative "GpxMerged"
+
+log = RkGpxLogger.new(STDOUT)
+
+# Parse commandline arguments and prepare output directory
+cmd = Commandline.new(ARGV)
+cmd.parse
+log.debug cmd.to_s
+
+
+gpx_merged = GpxMerged.new(cmd.files, log)
