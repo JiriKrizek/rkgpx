17f17eb63aaccc3cc1452a315cfaa66c9f014994
JiriKrizek <jkrizek@gmail.com>
2012-07-22 18:36:10 +0200

    Main.rb - pridani logovani

--- lib/main.rb
+++ lib/main.rb
@@ -3,6 +3,7 @@ require_relative "commandline"
 require_relative "gpx"
 require_relative "GpxMerged"
 
+# Enable logging
 log = RkGpxLogger.new(STDOUT)
 
 # Parse commandline arguments and prepare output directory
