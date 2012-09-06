a645b2f307eb206996b8b20dfb20fb6ab849396e
JiriKrizek <jkrizek@gmail.com>
2012-09-06 23:14:43 +0200

    Serazeni gpxek podle casu

--- lib/GpxMerged.rb
+++ lib/GpxMerged.rb
@@ -18,22 +18,36 @@ class GpxMerged
         raise ArgumentError.new("Either option :output_dir or option :in_place must be provided")
       end
 
+      @gpx_output=[]
+
       @files=files
       @files.each { |file|
         if File.readable?(file)
           gpx = Gpx.new(file, @log)
+          @log.debug "=== Processing file #{gpx.filename}"
 
           begin
             gpx.fix_trkseg
           rescue XmlParseError => e
             @log.warn "Fixed trkseg for file '#{gpx.filename}' with error #{e.message}"
+            return
           end
+
+          @log.debug gpx.get_gpx_type
 
           #dont save for now TODO
           #gpx.save_in_place
         else
           log.info "File #{file} is not readable or does not exist. Skipping."
         end
+        @gpx_output << gpx
+        @log.debug "=== File #{gpx.filename} processed"
+      }
+
+      @gpx_output.sort_by!(&:get_gpx_date)
+
+      @gpx_output.each { |g|
+        @log.debug "#{g.filename}: #{g.get_gpx_date}"
       }
     end
 end
