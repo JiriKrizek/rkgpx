8339ac2af7ee509a3bcf8c42e1f9527801b5c2a2
JiriKrizek <jkrizek@gmail.com>
2012-09-12 06:55:43 +0200

    Seznam gpx je serazeny a unikatni podle data, ukladani do vystupniho souboru probiha pro kazdou souvislou cestu (dle threshold)

--- lib/GpxMerged.rb
+++ lib/GpxMerged.rb
@@ -21,10 +21,11 @@ class GpxMerged
       raise ArgumentError.new("Either option :output_dir or option :in_place must be provided")
     end
 
-    @hash=Hash.new
-
     @threshold = threshold
     @files=files
+
+    hash = Hash.new
+    # Generate Gpx object for each file
     @files.each { |file|
       if File.readable?(file)
         gpx = Gpx.new(file, @log)
@@ -33,6 +34,7 @@ class GpxMerged
         begin
           gpx.fix_trkseg
         rescue XmlParseError => e
+          # Skip file in case of error
           @log.warn "trkseg for file '#{gpx.filename}' not fixed with error #{e.message}"
           next
         end
@@ -44,35 +46,24 @@ class GpxMerged
 
         @log.debug "Starting fix_timestamps:"
         gpx.fix_timestamps
-
-        #dont save for now TODO
-        #gpx.save_in_place
       else
         log.info "File #{file} is not readable or does not exist. Skipping."
       end
-      unless @hash.has_key?(gpx.gpx_time.to_s)
-        @hash[gpx.gpx_time.to_s] = gpx
+
+      # Add file to hash only if there is no file with same gpx_time
+      unless hash.has_key?(gpx.gpx_time.to_s)
+        hash[gpx.gpx_time.to_s] = gpx
       else
         @log.warn "GPX file with start time #{gpx.gpx_time.to_s} already defined, ignoring."
       end
+
       @log.debug "=== File #{gpx.filename} processed"
     }
     @log.debug "Finished processing of individual gpx files."
 
-    @gpx_output = Array.new
-    @hash.each { |key, value|
-      @gpx_output << value
-    }
+    @gpx_output = sorted_uniq_gpx(hash)
 
-    @gpx_output.each {|g|
-      g.filename
-    }
-
-    @gpx_output.sort_by!(&:gpx_time)
-
-    @log.debug "GPX files sorted."
-
-    activities = Array.new
+    activities = []
     activities << @gpx_output[0] unless @gpx_output.empty?
 
     @gpx_output.each_with_index { |gpx, index|
@@ -88,14 +79,74 @@ class GpxMerged
         dst = distance(first, last)["m"].round(2)
         if dst > @threshold
           @log.warn "Distance #{dst} meters is bigger than defined threshold (#{@threshold} meters). Consider change threshold using switch '-t number'"
+
+          # Merge and continue
+          xml_result_output = merge_gpx_files(activities)
+
+          # Save output to file
+          unless activities.empty?
+            save_to_file(xml_result_output, activities)
+            activities = []
+          else
+            @log.debug "Activities are empty (GpxMerged.rb)"
+          end
+
         else
           activities << gpx
           @log.debug "Distance #{dst} m <= threshold #{@threshold} m, adding activity #{gpx.filename}"
-          activities.each { |a|
-            @log.debug "\t\t#{a.filename}"
-          }
         end
       end
     }
+
+    unless activities.empty?
+      @log.debug "Merging rest of activities"
+      xml_result_output = merge_gpx_files(activities)
+
+      save_to_file(xml_result_output, activities)
+    end
+  end
+
+  # Merge sorted array of Gpx objects and return indented xml output
+  def merge_gpx_files(sorted_array)
+    raise ArgumentError.new("Invalid argument #{sorted_array.class}") unless sorted_array.kind_of? Array
+
+    first = sorted_array.shift
+
+    sorted_array.each { |a|
+      # Appends <trkseg> from next gpx after last <trkseg> in first file
+      first_trk=first.gpx_ns_all_trkseg
+      trkseg=a.gpx_all_trkseg
+
+      first_trk.after(trkseg)
+    }
+
+    # Converts to XML and returns
+    first.xml_doc.to_xml(:indent => 2)
+  end
+
+  private
+  def sorted_uniq_gpx(hash)
+    gpx_output = []
+
+    hash.each { |key, value|
+      gpx_output << value
+    }
+
+    @log.debug "GPX files are now sorted and uniq."
+
+    gpx_output.sort_by(&:gpx_time)
+  end
+
+  def save_to_file(output_str, act_array)
+    filename="output_"
+    filename+= act_array.last.gpx_timestamp.to_s.gsub(":", "-").gsub(" ", ".")
+    filename+=".gpx"
+
+    @log.debug "Adding these gpx to #{filename}:"
+    act_array.each { |a|
+      @log.debug "\t\t#{a.filename}"
+    }
+
+    File.open(filename, 'w') { |f| f.write(output_str) }
   end
 end
