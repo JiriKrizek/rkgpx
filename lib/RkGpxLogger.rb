550c49ffd79b11351f58f9954f53a068738038e4
JiriKrizek <jkrizek@gmail.com>
2012-09-12 06:56:14 +0200

    Logovani zaroven do souboru a na obrazovku

--- lib/RkGpxLogger.rb
+++ lib/RkGpxLogger.rb
@@ -1,10 +1,11 @@
 require 'logger'
+require_relative 'MultiIO'
 
 class RkGpxLogger < Logger
   FORMAT="%H:%M:%S"
 
   def initialize(output, lev=Logger::DEBUG)
-  	super(output, 'daily')
+    super(MultiIO.new(File.open(output,'w'), STDOUT), 'daily')
     self.level = lev
     self.datetime_format = FORMAT
     self.debug("Starting #{$PROGRAM_NAME}")
