24fe1b3df305e714d5feaf2960de586d4023c05b
JiriKrizek <jkrizek@gmail.com>
2012-09-12 07:02:59 +0200

    Logovani pouze od urovne INFO

--- lib/RkGpxLogger.rb
+++ lib/RkGpxLogger.rb
@@ -4,11 +4,10 @@ require_relative 'MultiIO'
 class RkGpxLogger < Logger
   FORMAT="%H:%M:%S"
 
-  def initialize(output, lev=Logger::DEBUG)
+  def initialize(output, lev=Logger::INFO)
     super(MultiIO.new(File.open(output,'w'), STDOUT), 'daily')
     self.level = lev
     self.datetime_format = FORMAT
     self.debug("Starting #{$PROGRAM_NAME}")
   end
-  
 end
